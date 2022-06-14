ecdd09f8c63422a71fc26c43b6cf806fb8de65d3
gabe <51098458+gabegaghi@users.noreply.github.com>
2022-06-14 16:55:41 -0400

    Starting nav bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import logo from './logo.svg';
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 import './App.css';
 
 function App() {
@@ -6,6 +9,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Navbar className="Nav-bar" fixed='top'>
+          <Nav className="Navigation">
+            <Nav.Link className="Nav-link" href="#home">Home</Nav.Link>
+            <Nav.Link className="Nav-link" href="#about">About</Nav.Link>
+            <Nav.Link className="Nav-link" href="#projects">Experience/Projects?</Nav.Link>
+            <Nav.Link className="Nav-link" href="#contact">Contact</Nav.Link>
+            <Nav.Link className="Nav-link" href="#links">Links?</Nav.Link>
+            {/*Last ones are separate from rest*/}
+            <Nav.Link className="Nav-link" href="#dev-info">About this page</Nav.Link>
+            <Nav.Link className="Nav-link" href="#accessibility">Accessibility</Nav.Link>
+          </Nav>
+        </Navbar>
       </header>
     </div>
   );
