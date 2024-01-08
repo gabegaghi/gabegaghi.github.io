0d1df743fc2b76d6e2dbb6c1c99173a86239c91c
gabe <51098458+gabegaghi@users.noreply.github.com>
2024-01-08 01:23:14 -0500

    update home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,19 +4,16 @@ import Alert from './Alert';
 import './Home.css';
 
 function Home() {
-  const [cityHover, setCityHover] = useState('Tkaronto');
   const [nameHover, setNameHover] = useState('Gabe');
+  const [btnHover, setBtnHover] = useState(false);
   return (
     <main className='home page'>
       <Alert/>
       <div className='intro'>
-        <h1 className='intro-title'>Hey, I'm <mark className='hoverable' onPointerEnter={()=> setNameHover("Gabriela")} onPointerLeave={() => setNameHover("Gabe")}>{nameHover}</mark> Esquivel Gaghi</h1>
-        <h2 className='intro-subtitle'>A newly graduated software developer with a focus on front-end design</h2>
-        <h3 className='intro-text'>I also make logos and other graphic designs, including some used in my <NavLink className='projects link' to='/projects'>projects</NavLink>!</h3>
-        <div className='availability'>
-          <box-icon class='location-icon' size='30px' name='map' type='solid' color='#f03333'></box-icon>
-          <p className='availability-text'>Available full-time for remote opportunities or in-person in <mark className='hoverable' onPointerEnter={() => setCityHover('Toronto')} onPointerLeave={() => setCityHover('Tkaronto')}>{cityHover}</mark></p>
-        </div>
+        <h1 className='intro-title'>Hey, I'm <mark className='hoverable' onPointerEnter={()=> setNameHover("Gabriela")} onPointerLeave={() => setNameHover("Gabe")}>{nameHover}</mark> Esquivel Gaghi!</h1>
+        <h2 className='intro-subtitle'>Newly graduated software developer with a focus on front-end design within a holistic approach. </h2>
+        <p className='intro-text'>I also make logos and other graphic designs, some of which have been used in my <NavLink className='projects link' to='/projects'>projects</NavLink>!</p>
+        <NavLink to='/about' className='about-button' onPointerEnter={() => setBtnHover(true)} onPointerLeave={() => setBtnHover(false)}><span>Learn more <b>about me</b></span><box-icon name='right-arrow-alt' animation={ btnHover ? 'fade-right' : 'none'} color='rgb(255, 225, 255)'></box-icon></NavLink>
       </div>
     </main>
   );
