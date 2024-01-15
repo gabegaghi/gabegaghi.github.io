92a51f1457202254e3579498457298c95a78c8b4
gabe <51098458+gabegaghi@users.noreply.github.com>
2024-01-15 02:08:31 -0500

    hover fix and home button styling

--- src/Components/Hover.js
+++ src/Components/Hover.js
@@ -1,14 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 function Hover({standard, hover}) {
+  const timerRef = useRef(null);
   const [value, setValue] = useState(standard);
 
   function setHover() {
-    value===standard? setValue(hover) : setValue(standard);
+    clearTimeout(timerRef.current);
+    setValue(hover);
+    timerRef.current = setTimeout(() => {
+      setValue(standard);
+    }, 1000);
   }
   
   return (
-    <mark className='hoverable' onClick={() => setHover()} onMouseEnter={() => setValue(hover)} onMouseLeave={() => {setValue(standard)}}>{value}</mark>
+    <mark className='hoverable' onClick={() => setHover()} onMouseEnter={() => setValue(hover)} onMouseLeave={() => setValue(standard)}>{value}</mark>
   )
 }
 
